87f275295dade959e9e8c06f4b1ef6f7bc8c5e38
Emily Creamer <emily.creamer@icloud.com>
2019-06-11 16:46:40 -0700

    app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
-import CreateNote from '../container/notes/CreateNote';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Home from './Home';
+import Callback from '../container/auth/Callback';
+import { withSession } from '../container/auth/withSession';
 
 export default function App() {
-  return <CreateNote />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={withSession(Home)} />
+        <Route path="/callback" component={Callback} />
+      </Switch>
+    </Router>
+  );
 }
